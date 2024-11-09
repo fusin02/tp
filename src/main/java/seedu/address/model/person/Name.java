2ee88a4825874f1cf38db84f90fc7212f81dca1d
fusin <fusin02@gmail.com>
2024-11-10 02:40:56 +0800

    Fix case-sensitivity check

--- src/main/java/seedu/address/model/person/Name.java
+++ src/main/java/seedu/address/model/person/Name.java
@@ -68,7 +68,7 @@ public class Name {
         }
 
         Name otherName = (Name) other;
-        return fullName.equals(otherName.fullName);
+        return fullName.equalsIgnoreCase(otherName.fullName);
     }
 
     @Override
